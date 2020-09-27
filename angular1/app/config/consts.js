8cd48bc704ab215ad72e142c97a7bac51fb23e18
Mauricio Carvalho <developer.mauricio1@gmail.com>
2020-09-27 01:07:29 -0300

    Mudança nas constantes em consts.js

--- angular1/app/config/consts.js
+++ angular1/app/config/consts.js
@@ -1,8 +1,8 @@
 angular.module('primeiraApp').constant('consts', {
   appName: 'MEAN - Primeira Aplicação',
-  version: '1.0',
-  owner: 'Cod3r',
-  year: '2017',
+  version: '1.1',
+  owner: 'Mauricio Carvalho',
+  year: '2020',
   site: 'http://cod3r.com.br',
   apiUrl: 'http://localhost:3003/api',
 }).run(['$rootScope', 'consts', function($rootScope, consts) {
